673a4a027e1c609a4975dcaf7d832d3852576f1f
justinfinkle <doubledaffy888@gmail.com>
2018-02-22 11:57:15 -0600

    Add code to compile simulation results

---
+++ scripts/compile_sim_results.py
@@ -0,0 +1,13 @@
+import os
+import pandas as pd
+from pydiffexp.gnw.results import GnwNetResults
+
+if __name__ == '__main__':
+    pd.set_option('display.width', 2000)
+    data_dir = "../data/motif_library/gnw_networks/"
+
+    # Make a list of all the subdirectories
+    gnr = GnwNetResults(data_dir)
+    t = [0, 15, 30, 60, 120, 240, 480]
+    all_stats = gnr.compile_results(censor_times=t)
+    all_stats.to_csv('intermediate_data/sim_stats_censoredtimes.tsv', sep='\t')
