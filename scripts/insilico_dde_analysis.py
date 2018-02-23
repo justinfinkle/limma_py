6971d3373d318c16964f0b3f0c7032961bcbb3c0
justinfinkle <doubledaffy888@gmail.com>
2018-02-23 08:34:25 -0600

    Do DDE analysis for insilico data

---
+++ scripts/insilico_dde_analysis.py
@@ -0,0 +1,31 @@
+import ast
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from pydiffexp import DEAnalysis
+
+pd.set_option('display.width', 250)
+dea = pd.read_pickle('intermediate_data/strongly_connected_dea.pkl')        # type: DEAnalysis
+der = dea.results['ko-wt']
+scores = der.score_clustering()
+scores = scores.loc[der.top_table()['adj_pval'] < 0.05]
+
+# Remove clusters that have no dynamic DE (i.e. all 1, -1, 0)
+interesting = scores.loc[scores.Cluster.apply(ast.literal_eval).apply(set).apply(len) > 1]
+
+# Heatmap of expression
+de_data = (der.top_table().iloc[:, :7]).multiply(der.p_value < 0.05)
+sort_idx = scores.sort_values(['Cluster', 'score'], ascending=False).index.values
+hm_data = de_data.loc[sort_idx]
+hm_data = hm_data.divide(hm_data.abs().max(axis=1), axis=0)
+
+cmap = sns.diverging_palette(30, 260, s=80, l=55, as_cmap=True)
+plt.figure(figsize=(4, 8))
+sns.heatmap(hm_data, xticklabels=dea.times, yticklabels=False, cmap=cmap)
+plt.xticks(rotation=90)
+plt.title('In silico DDE')
+plt.tight_layout()
+plt.show()
+
+print(interesting.sort_values(['Cluster', 'score'], ascending=False))
